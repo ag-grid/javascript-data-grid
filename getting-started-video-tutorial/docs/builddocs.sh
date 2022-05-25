3dbd11d4a696b882cb47224249f1866eb509a92a
Alan Richardson <alan.richardson@ag-grid.com>
2022-05-25 15:35:36 +0100

    added vid 3

--- getting-started-video-tutorial/docs/builddocs.sh
+++ getting-started-video-tutorial/docs/builddocs.sh
@@ -8,8 +8,8 @@ cat 001-quickstart-javascript-ag-grid.md >> javascript-tutorial.md
 cat newpage.md >> javascript-tutorial.md
 cat 002-enable-enterprise.md >> javascript-tutorial.md
 
-#cat newpage.md >> javascript-tutorial.md
-#cat 003-javascript-cell-renderers.md >> javascript-tutorial.md
+cat newpage.md >> javascript-tutorial.md
+cat 003-javascript-cell-renderers.md >> javascript-tutorial.md
 
 cat newpage.md >> javascript-tutorial.md
 cat 999-outro.md >> javascript-tutorial.md
